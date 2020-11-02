83c7b7a64ee70e5ed101c13ac44a7314116ac9c8
lndrcrvlh <lndrcrvlh@gmail.com>
2020-11-02 09:07:24 -0300

    Adds comment in Behavior model about naming

--- backend/models/behavior.model.js
+++ backend/models/behavior.model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+//what I call here 'behavior' should really be
+//called program in technical terms, and in the
+//interface in future should be referred to as such
+
 const behaviorSchema = new Schema(
     {
         name: {type:String, required:true},
