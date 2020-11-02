72d4c6b4ce04502909147e37506c6c1fd31d93a7
lndrcrvlh <lndrcrvlh@gmail.com>
2020-11-02 09:39:33 -0300

    Completes Behavior list feature

--- src/components/behavior-list.component.js
+++ src/components/behavior-list.component.js
@@ -1,14 +1,77 @@
-import React,{ Component } from "react";
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-class ListBehavior extends Component{
-    constructor(props){
-        super(props)
-    }
+const Behavior = (props) => (
+  <tr>
+    <td>{props.behavior.name}</td>
+    <td>{props.behavior.objective}</td>
+    <td>{props.behavior.setting}</td>
+    <td>{props.behavior.reinforcement_schedule}</td>
+    <td>{props.behavior.materials_required}</td>
+    <td>{props.behavior.program_procedure}</td>
+    <td>{props.behavior.mastery_criteria}</td>
+  </tr>
+);
 
-    render(){
-        return(<h1>Behaviors Placeholder</h1>)
-    }
+class ListBehavior extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      behaviors: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/behaviors/")
+      .then((response) => {
+        this.setState({ behaviors: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  behaviorsList() {
+    return this.state.behaviors.map((currentBehavior) => {
+      return (
+        <Behavior
+          behavior={currentBehavior}
+          name={currentBehavior.name}
+          objective={currentBehavior.objective}
+          setting={currentBehavior.setting}
+          reinforcement_schedule={currentBehavior.reinforcement_schedule}
+          materials_required={currentBehavior.materials_required}
+          program_procedure={currentBehavior.program_procedure}
+          mastery_criteria={currentBehavior.mastery_criteria}
+          key={currentBehavior._id}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Behaviors</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Program</th>
+              <th>Objective</th>
+              <th>Setting</th>
+              <th>Reinforcement Schedule</th>
+              <th>Materials</th>
+              <th>Procedure</th>
+              <th>Mastery Criteria</th>
+            </tr>
+          </thead>
+          <tbody>{this.behaviorsList()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default ListBehavior;
