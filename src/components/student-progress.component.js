097b2866af626f060e9e787bc5fe9dfb983b81b3
lndrcrvlh <lndrcrvlh@gmail.com>
2020-11-16 12:24:16 -0300

    Students dropdown in student-progress component works statically

--- src/components/student-progress.component.js
+++ src/components/student-progress.component.js
@@ -1,104 +1,23 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import Select from "react-select";
 
-export default class StudentProgress extends Component {
-  constructor(props) {
-    super(props);
+const options = [
+  { _id: "5fa026645bf07b1fc4c08a46", s_name: "me", s_age: 334, __v: 0 },
+  { _id: "5fa026f55bf07b1fc4c08a47", s_name: "hello", s_age: 4, __v: 0 },
+  { _id: "5fa2ff62aedc4ff986e203f5", s_name: "bobby", s_age: 12, __v: 0 },
+];
 
-    this.onChangeStudent = this.onChangeStudent.bind(this);
-    this.onChangeStudents = this.onChangeStudents.bind(this);
-    this.onChangeBehavior = this.onChangeBehavior.bind(this);
-    this.onChangeBehaviors = this.onChangeBehaviors.bind(this);
+export default function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-    this.state = {
-      student: {},
-      behavior: {},
-      trials: {},
-      students: [],
-      behaviors: [],
-    };
-  }
-
-  getStudents() {
-    return axios.get("http://localhost:5000/students");
-  }
-
-  getBehaviors() {
-    return axios.get("http://localhost:5000/behaviors");
-  }
-
-  onChangeStudents(e) {
-    this.setState({
-      students: e.target.value,
-    });
-  }
-
-  onChangeStudent(e) {
-    this.setState({
-      student: e.target.value,
-    });
-  }
-
-  onChangeBehavior(e) {
-    this.setState({
-      behavior: e.target.value,
-    });
-  }
-
-  onChangeBehaviors(e) {
-    this.setState({
-      behaviors: e.target.value,
-    });
-  }
-
-  componentDidMount() {
-    Promise.all([this.getStudents(), this.getBehaviors()]).then((response) => {
-      if (response[1].data.length) {
-        this.setState({
-          students: response[0].data.map((student) => student.s_name),
-          student: {
-            s_name: response[0].data[0].s_name,
-            s_id: response[0].data[0]._id,
-          },
-        });
-      }
-      if (response[1].data.length) {
-        this.setState({
-          behaviors: response[1].data.map((behavior) => behavior.name),
-          behavior: {
-            b_name: response[1].data[0].name,
-            b_id: response[1].data[0]._id,
-          },
-        });
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>Create a relationship</h3>
-        <form>
-          <div>
-            <label>Alunos: </label>
-            <select
-              ref="userInput"
-              required
-              className="form-control"
-              //value={this.state.student.s_name}
-              onChange={this.onChangeStudents}
-            >
-              {this.state.students.map((s) => {
-                return (
-                  <option key={s} value={s}>
-                    {s}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="App bg-dark text-light">
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+        getOptionLabel={option => `${option.s_name}`}
+      />
+    </div>
+  );
 }
