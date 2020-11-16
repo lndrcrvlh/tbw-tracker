c7494a2b114d8d54919af01bc9bec654a0d8fcfe
lndrcrvlh <lndrcrvlh@gmail.com>
2020-11-16 18:15:36 -0300

    Dynamically render behaviors in dropdown on student-progress comp

--- src/components/student-progress.component.js
+++ src/components/student-progress.component.js
@@ -1,87 +1,80 @@
-import React, { useState, Component } from "react";
+import React, { Component } from "react";
 import Select from "react-select";
 import axios from "axios";
-
-// const options = [
-//   { _id: "5fa026645bf07b1fc4c08a46", s_name: "me", s_age: 334, __v: 0 },
-//   { _id: "5fa026f55bf07b1fc4c08a47", s_name: "hello", s_age: 4, __v: 0 },
-//   { _id: "5fa2ff62aedc4ff986e203f5", s_name: "bobby", s_age: 12, __v: 0 },
-//];
 
 export default class StudentProgress extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedOption: [],
+      selectBehaviors: [],
+      b_name: "",
+      b_id: "",
+      selectStudents: [],
       s_name: "",
       s_id: "",
     };
   }
 
-  async getOptions() {
+  async getStudents() {
     const res = await axios.get("http://localhost:5000/students");
     const data = res.data;
 
-    const options = data.map((d) => ({
+    const students = data.map((d) => ({
       value: d._id,
       label: d.s_name,
     }));
 
-    this.setState({ selectOptions: options });
+    this.setState({ selectStudents: students });
   }
 
-  handleChange(e) {
-    this.setState({ id: e.value, name: e.label });
+  async getBehaviors() {
+    const res = await axios.get("http://localhost:5000/behaviors");
+    const data = res.data;
+
+    const behaviors = data.map((d) => ({
+      value: d._id,
+      label: d.name,
+    }));
+
+    this.setState({ selectBehaviors: behaviors });
+  }
+
+  handleChangeBehavior(e) {
+    this.setState({ b_id: e.value, b_name: e.label });
+  }
+
+  handleChangeStudent(e) {
+    this.setState({ s_id: e.value, s_name: e.label });
   }
 
   componentDidMount() {
-    this.getOptions();
+    this.getStudents();
+    this.getBehaviors();
   }
 
   render() {
-    console.log(this.state.selectOptions);
+    console.log(this.state.selectStudents, "-----");
     return (
       <div>
         <Select
-          options={this.state.selectOptions}
-          onChange={this.handleChange.bind(this)}
+          options={this.state.selectStudents}
+          onChange={this.handleChangeStudent.bind(this)}
         />
         <p>
-          You have selected <strong>{this.state.name}</strong> whose id is{" "}
-          <strong>{this.state.id}</strong>
+          You have selected <strong>{this.state.s_name}</strong> whose id is{" "}
+          <strong>{this.state.s_id}</strong>
         </p>
+        <Select
+          options={this.state.selectBehaviors}
+          onChange={this.handleChangeBehavior.bind(this)}
+        />
+        <p>
+          You have selected <strong>{this.state.b_name}</strong> whose id is{" "}
+          <strong>{this.state.b_id}</strong>
+        </p>
+        
       </div>
     );
   }
 }
-
-// export default function App() {
-//   const [selectedOption, setSelectedOption] = useState(null);
-
-//   //console.log(options, "-=-=0-0=-0=-0=-0=-0=-0-09=89=98-09");
-
-//   const findOptions = () => {
-//     return axios
-//       .get("http://localhost:5000/students")
-//       .then((response) => setOptions(response.data))
-//       .catch((err) => console.log(err));
-//   };
-
-//   let options = [];
-
-//   const setOptions = (res) => {
-//     return (options = res);
-//   };
-
-//   return (
-//     <div className="App bg-dark text-light">
-//       <Select
-//         defaultValue={selectedOption}
-//         onChange={setSelectedOption}
-//         options={findOptions()}
-//         getOptionLabel={(option) => `${option.s_name}`}
-//       />
-//     </div>
-//   );
-// }
