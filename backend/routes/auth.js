79576e612fc82c753effe5ba70c9fb19454d7296
Tyler Reid <Tylerreid1988@gmail.com>
2021-09-01 16:18:11 -0400

    save user in database, add 'user already exists' functionality. ALL BACKEND ONLY

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,4 +7,34 @@ router.get('/signup', (req,res) =>{
   res.send('hello');
 })
 
+router.post('/signup', (req, res) => {
+  const {name, email,password}=req.body
+  console.log(req.body)
+  if(!email || !name || !password) {
+    return res.status(422).json({error:'Please Fill out all Fields'})
+  }
+  User.findOne({email:email})
+  .then((savedUser)=>{
+    if(savedUser){
+      return res.status(422).json({error:'User already Exists'})
+    }
+    const user=new User({
+      email,
+      password,
+      name,
+    })
+    user.save()
+    .then((user)=>{
+      res.json({message:'Saved Successfully'})
+      console.log(user.email)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+})
+
 module.exports = router
