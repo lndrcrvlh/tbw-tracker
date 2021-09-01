535be68f6179c10feccf35280ad1ced6a8e7898a
Tyler Reid <Tylerreid1988@gmail.com>
2021-09-01 16:04:16 -0400

    Setup API routes for login page, Add User model, auth file and require them in server

---
+++ backend/routes/auth.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+
+router.get('/signup', (req,res) =>{
+  res.send('hello');
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-
+require('./models/user')
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(require('./routes/auth'))
 
 const uri = process.env.URI;
 mongoose.connect(uri, {
